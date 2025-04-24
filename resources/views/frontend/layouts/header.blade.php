dea4fd88ff1e085c82f963aa4ba4fe9941c75287
cuong1211 <cuonghaha43@gmail.com>
2025-04-24 17:55:35 +0700

    first commit

---
+++ resources/views/frontend/layouts/header.blade.php
@@ -0,0 +1,76 @@
+<div class="preloader">
+    <div class="loading-container">
+        <div class="loading"></div>
+        <div id="loading-icon"><img src="images/loader.svg" alt=""></div>
+    </div>
+</div>
+<!-- Preloader End -->
+
+<!-- Header Start -->
+<header class="main-header">
+    <div class="header-sticky">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <!-- Logo Start -->
+                <a class="navbar-brand" href="./">
+                    <img src="images/logo.svg" alt="Logo">
+                </a>
+                <!-- Logo End -->
+
+                <!-- Main Menu Start -->
+                <div class="collapse navbar-collapse main-menu">
+                    <div class="nav-menu-wrapper">
+                        <ul class="navbar-nav mr-auto" id="menu">
+                            <li class="nav-item submenu"><a class="nav-link" href="./">Home</a>
+                                <ul class="sub-menu">
+                                    <li class="nav-item"><a class="nav-link" href="index.html">Home - Hero Version
+                                            1</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="index-2.html">Home - Hero Version
+                                            2</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="index-3.html">Home - Hero Version
+                                            3</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="index-4.html">Home - Hero Version
+                                            4</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="index-5.html">Home - Hero Version
+                                            5</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item"><a class="nav-link" href="about.html">About Us</a></li>
+                            <li class="nav-item"><a class="nav-link" href="service.html">Services</a></li>
+                            <li class="nav-item submenu"><a class="nav-link" href="#">Pages</a>
+                                <ul>
+                                    <li class="nav-item"><a class="nav-link" href="service-single.html">Service
+                                            Details</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="blog.html">Our Blog</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="blog-single.html">Blog
+                                            Details</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="team.html">Our Team</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="team-single.html">Team
+                                            Details</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="gallery.html">Gallery</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="technology.html">Technology</a>
+                                    </li>
+                                    <li class="nav-item"><a class="nav-link"
+                                            href="testimonials.html">testimonials</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="faqs.html">FAQ's</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="404.html">404</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item"><a class="nav-link" href="contact.html">Contact Us</a></li>
+                            <li class="nav-item highlighted-menu"><a class="nav-link" href="appointment.html">book
+                                    appointment</a></li>
+                        </ul>
+                    </div>
+                    <!-- Let’s Start Button Start -->
+                    <div class="header-btn d-inline-flex">
+                        <a href="appointment.html" class="btn-default">book appointment</a>
+                    </div>
+                    <!-- Let’s Start Button End -->
+                </div>
+                <!-- Main Menu End -->
+                <div class="navbar-toggle"></div>
+            </div>
+        </nav>
+        <div class="responsive-menu"></div>
+    </div>
+</header>
---
+++ resources/views/frontend/layouts/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    @include('frontend.layouts.source')
+</head>
+
+<body>
+    @include('frontend.layouts.header')
+    <section>
+        @yield('content')
+    </section>
+    <!-- Footer Start -->
+    @include('frontend.layouts.footer')
+    <!-- Footer End -->
+    @yield('js')
+    @stack('jscustom')
+
+</body>
+
+</html>
