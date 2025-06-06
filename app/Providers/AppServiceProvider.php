243583b855c9b645f417c68bd46c9128617ce54f
cuong <cuonghaha43@gmail.com>
2025-06-06 23:43:53 +0700

    add

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,18 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    public function boot()
     {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
+        // Loại bỏ /public/ khỏi URL
+        URL::forceRootUrl(config('app.url'));
+        
+        // Cấu hình public path
+        $this->app->bind('path.public', function() {
+            return base_path();
+        });
     }
 }
